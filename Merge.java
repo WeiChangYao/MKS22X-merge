1e2092f2a8965b90bd09c8d1d0e9aff185b3eabb
weichangyao <wyao1@stuy.edu>
2019-03-18 11:44:44 -0400

    added Merge

--- Merge.java
+++ Merge.java
@@ -1,7 +1,6 @@
 public class Merge{
   public static void mergesort(int[]data){
-
-
+    return mergesort(data,0,data.length-1);//Length MINUS ONE?????????
   }
   private static void mergesort(int[]data, int lo, int hi){
     if (hi <= lo){
@@ -9,7 +8,7 @@ public class Merge{
     }
     /*int mid = 0;
     if (data.length%2 == 1){
-      mid = data.length/2;//add one?
+      mid = data.length/2;
     }
     else{
       mid = data.length/2-1;
