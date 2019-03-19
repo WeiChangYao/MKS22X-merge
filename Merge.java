d241fd51fc792fb36fbcc51d82b872c419fcfb0a
weichangyao <wyao1@stuy.edu>
2019-03-19 11:48:46 -0400

    worked on merge

--- Merge.java
+++ Merge.java
@@ -1,20 +1,48 @@
 public class Merge{
   public static void mergesort(int[]data){
-    return mergesort(data,0,data.length-1);//Length MINUS ONE?????????
+    mergesort(data,0,data.length-1);//Length MINUS ONE?????????
   }
   private static void mergesort(int[]data, int lo, int hi){
     if (hi <= lo){
       return;
     }
-    /*int mid = 0;
-    if (data.length%2 == 1){
-      mid = data.length/2;
-    }
-    else{
-      mid = data.length/2-1;
-    }*/
-    mergesort(data,lo,lo+(hi-lo)/2);//left
-    mergesort(data,lo+1,hi);//right
-
+    int mi = lo+(hi-lo)/2;
+    mergesort(data,lo,mi);//left
+    mergesort(data,mi+1,hi);//right
+    //merge(data,lo,mid,hi);
   }
+  private static void merges(int[] data, int low, int mid, int hig){
+    int one = mid-low+1;
+    int two = hig - mid;
+    int[] ary = new int[one];
+    int[] ary2 = new int[two];
+    for(int i = 0; i < mid; i++){
+      ary[i]=data[i];
+    }
+    for(int i = 0; mid+1 < hig; i++){
+      ary2[i]=data[i];
+      mid++;
+    }
+    int[] ary3 = new int[hig+1];
+    int c = 0;
+    int c2 = 0;
+    int c3 = 0;
+    while(c < ary.length & c2 < ary2.length){
+      if (ary[c] < ary2[c2]){
+        ary3[c3] = ary[c];
+        c++;
+        c3++;
+      }
+      else{
+        ary[c3] = ary2[c2];
+        c2++;
+        c3++;
+      }
+    }
+    while(c < ary.length){
+      
+    }
+  }
+
+
 }
